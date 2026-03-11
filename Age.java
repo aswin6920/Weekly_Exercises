d0aaebab5df724188905f20051fcb86df74f30fa
aswin6920 <iam.aswinkarthik@gmail.com>
2026-03-11 15:07:00 +0530

    Week_3_4

--- Age.java
+++
@@ -1,8 +0,0 @@
-public class Age
-{
-    public static void main(String[] args)
-    {
-        int current = 2024, harry = 2000;
-        System.out.println("Harry's age in 2024 is " + (current - harry));
-    }
-}
